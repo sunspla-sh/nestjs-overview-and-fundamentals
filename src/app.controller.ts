48934dcface9ed8f140c486e83e01e71954521d9
jack <jrfisch95@gmail.com>
2023-05-17 09:23:38 -0500

    add all exception filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  UseFilters,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AllExceptionsFilter } from './filters/all-exception.filter';
 
 @Controller()
+@UseFilters(AllExceptionsFilter)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test-all-exceptions-filter')
+  getThrow(): string {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 }
---
+++ src/filters/all-exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    //In certain situations the httpAdapter might not be available in the constructor
+    //so we should resolve it here
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      haha: 'hi',
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
